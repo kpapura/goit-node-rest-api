ad78274e23db99980e9800b15a5154d10c726783
kpapura <125825500+kpapura@users.noreply.github.com>
2024-04-27 12:32:18 +0300

    completed task

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -4,17 +4,20 @@ import { createContactSchema, updateContactSchema } from "../schemas/contactsSch
 import contactsControllers from "../controllers/contactsControllers.js";
 import validateBody from "../helpers/validateBody.js"
 import isEmptyBody from "../middlewares/isEmptyBody.js";
+import isValidId from "../middlewares/isValidId.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsControllers.getAllContacts);
 
-contactsRouter.get("/:id", contactsControllers.getOneContact);
+contactsRouter.get("/:id", isValidId, contactsControllers.getOneContact);
 
-contactsRouter.delete("/:id", contactsControllers.deleteContact);
+contactsRouter.delete("/:id", isValidId, contactsControllers.deleteContact);
 
 contactsRouter.post("/", isEmptyBody, validateBody(createContactSchema), contactsControllers.createContact);
 
-contactsRouter.put("/:id", isEmptyBody, validateBody(updateContactSchema), contactsControllers.updateContact);
+contactsRouter.put("/:id", isValidId, isEmptyBody, validateBody(updateContactSchema), contactsControllers.updateContact);
+
+contactsRouter.put("/:id/favorite", isValidId, isEmptyBody, contactsControllers.updateStatusContact);
 
 export default contactsRouter;
